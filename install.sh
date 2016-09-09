096676bd2097ba3cfb24c8205d4be3b431925e0c
Kris Applesauce <kris@nivenly.com>
2016-09-09 06:52:19 -0600

    Emacs

--- install.sh
+++ install.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-# Install script for the repository. Basically this will just add our bin to $PATH
+# Install emacs
+brew install emacs # Will install emacs24
 
+# Install script for the repository. Basically this will just add our bin to $PATH
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 echo "export PATH=$PATH:$DIR/bin" >> ~/.bash_profile
+echo "export EDITOR=/usr/bin/emacs" >> ~/.bash_profile
+echo "alias emacs=/usr/local/bin/emacs" >> ~/.bash_profile

bda1d1f83dda2b5cb255ce79c8069417ab5797ab
Kris Childress <kris@nivenly.com>
2016-09-09 11:02:19 -0600

    Moar changes

--- install.sh
+++ install.sh
@@ -3,7 +3,6 @@
 # Install emacs
 brew install emacs # Will install emacs24
 
-# Install script for the repository. Basically this will just add our bin to $PATH
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 echo "export PATH=$PATH:$DIR/bin" >> ~/.bash_profile
 echo "export EDITOR=/usr/bin/emacs" >> ~/.bash_profile
